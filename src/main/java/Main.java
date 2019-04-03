5fca2c2da6414a1d673c4cbedfc1495f2a4ffa6a
ryudung <rhkdgus221@naver.com>
2019-04-03 22:21:19 +0900

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        //사용자 input 데이터
+        String input = scanner.nextLine();
+
+        //======작성 시작=======//
+
+
+        //======작성 끝=======//
+
+        scanner.close();
+    }
+}
